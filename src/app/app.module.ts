74e7836c9566f06dc8265c7dc64af97f83e2ee75
fuji.nguyen <fuji.nguyen@workcontrol.com>
2021-01-24 01:23:40 -0500

    ng add angular-auth-oidc-client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ShellModule } from './shell/shell.module';
 import { AboutModule } from './about/about.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthConfigModule } from './auth/auth-config.module';
 
 @NgModule({
   imports: [
@@ -30,11 +31,11 @@ import { AppRoutingModule } from './app-routing.module';
     HomeModule,
     AboutModule,
     AuthModule,
-    AppRoutingModule // must be imported as the last module as it contains the fallback route
+    AppRoutingModule,
+    AuthConfigModule, // must be imported as the last module as it contains the fallback route
   ],
   declarations: [AppComponent],
-  providers: [
-  ],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/auth/auth-config.module.ts
@@ -0,0 +1,32 @@
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { AuthModule, OidcConfigService } from 'angular-auth-oidc-client';
+
+export function configureAuth(oidcConfigService: OidcConfigService): () => Promise<any> {
+  return () =>
+    oidcConfigService.withConfig({
+      stsServer: 'https://devkit-sts.azurewebsites.net',
+      redirectUrl: window.location.origin,
+      postLogoutRedirectUri: window.location.origin,
+      clientId: 'please-enter-clientId',
+      scope: 'please-enter-scopes', // 'openid profile offline_access ' + your scopes
+      responseType: 'code',
+      silentRenew: true,
+      useRefreshToken: true,
+      renewTimeBeforeTokenExpiresInSeconds: 30,
+    });
+}
+
+@NgModule({
+  imports: [AuthModule.forRoot()],
+  exports: [AuthModule],
+  providers: [
+    OidcConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: configureAuth,
+      deps: [OidcConfigService],
+      multi: true,
+    },
+  ],
+})
+export class AuthConfigModule {}
